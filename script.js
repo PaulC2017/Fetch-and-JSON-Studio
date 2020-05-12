a46cee5b968b0dc4e0c913a1b0eb4cb05fca3bb6
Paul <paulc2017@att.net>
2020-05-11 23:13:11 -0400

    First comit for json studio

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    let json = [];
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            const astronaut = document.getElementById("astronaut");
+            json.forEach(function(astro) {
+                astronaut.innerHTML = astronaut.innerHTML + `
+
+            <div class="astronaut">
+               <div>
+               <h3>
+               ${astro.firstName} ${astro.lastName}
+               </h3>
+
+               <ul>
+                    <li>Hours in space: ${astro.hoursInSpace}</li>
+                    <li>Active: ${astro.active}</li>
+                    <li>Skills: ${astro.skills}</li>
+                </ul>
+                </div>
+                <img class ="avatar" src=${astro.picture} height=250></img>
+            </div>
+         `;
+
+            });
+
+
+        });
+    });
+
+
+});
+
+//
